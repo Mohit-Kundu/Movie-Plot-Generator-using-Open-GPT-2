a6fb2a54aad520e03d50a0e849de2a16f30078f2
Mohit-Kundu <moh8.kundu@gmail.com>
2021-10-13 17:33:19 +0530

    Updated conditions and filters for scraping data

--- plot_extractor.py
+++ plot_extractor.py
@@ -2,14 +2,15 @@ import pandas as pd
 
 dataset_name = "wiki_movie_plots_deduped.csv"
 column_name = "Plot"
+no_of_rows = 200
 
 df = pd.read_csv(dataset_name)
 
 #Applying conditions to Dataframe
-df = df.loc[df["Origin/Ethnicity"] == "American"]
+df = df.loc[df["Origin/Ethnicity"] == "American"].tail(no_of_rows)
 
 #Extracting plots to plots.txt
 with open("plots.txt", "a") as file:
-    for i in range(500):
-        data = "<BOL>"+str(df[column_name].iloc[i].encode("ascii","ignore"))+"<EOL>\n\n"
+    for i in range(no_of_rows):
+        data = "<BOL>"+str(df[column_name].iloc[i].encode("ascii","ignore"))+"<EOL>\n\n" #ignoring unicode characters
         file.write(data)
