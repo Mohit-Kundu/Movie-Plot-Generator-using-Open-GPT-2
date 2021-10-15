a3a2cbfd8d3ff63e4c26b9065c188abac35efa6d
Mohit-Kundu <moh8.kundu@gmail.com>
2021-10-16 02:10:54 +0530

    Updated plot_extractor.py

--- plot_extractor.py
+++ plot_extractor.py
@@ -3,7 +3,7 @@ import re
 
 dataset_name = "wiki_movie_plots_deduped.csv"
 column_name = "Plot"
-no_of_rows = 200
+#no_of_rows = 200
 
 def format_text(text):
     '''Formats text by removing citations and also unicode & newline characters'''
@@ -15,18 +15,18 @@ def format_text(text):
     text = re.sub(regex1, "'", text) #replaces \'s with 's
     text = re.sub("|".join([citations, regex2, regex3]) , "", text) #removes regex patterns from text
     text = text.encode("ascii","ignore").decode('UTF-8') #removes unicode characters & converting bytes to string
-    text = "<BOL>" + str(text) + "<EOL>\n\n"
+    text = "<|startoftext|>" + str(text) + "<|endoftext|>\n\n"
     return text
 
 
 df = pd.read_csv(dataset_name)
 
 #Applying conditions to Dataframe
-df = df.loc[df["Origin/Ethnicity"] == "American"].tail(no_of_rows)
+df = df.loc[df["Origin/Ethnicity"] == "American"]#.tail(no_of_rows)
 
 #Extracting plots to plots.txt
 with open("plots.txt", "a") as file:
-    for i in range(no_of_rows):
+    for i in range(df.shape[0]):
         raw_data = df[column_name].iloc[i]
         plot = format_text(raw_data)
         file.write(plot)
